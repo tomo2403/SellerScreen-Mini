0b42b172051c6da96b4fc5efd77209717361474f
Tom <tom.mohr@t-app-germany.com>
2021-05-13 20:37:32 +0200

    Initial commit

---
+++ SellerScreen-Mini/SellerScreen_Mini.lpr
@@ -0,0 +1,23 @@
+program SellerScreen_Mini;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, MainUnit
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Title:='SellerScreen-Mini';
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
+
---
+++ SellerScreen-Mini/mainunit.pas
@@ -0,0 +1,112 @@
+unit MainUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Buttons, ExtCtrls,
+  StdCtrls, Grids, ButtonPanel, ComCtrls, Menus;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    CancelBtn: TButton;
+    FlowPanel10: TFlowPanel;
+    Label8: TLabel;
+    MenuItem10: TMenuItem;
+    MenuItem11: TMenuItem;
+    MenuItem12: TMenuItem;
+    MenuItem13: TMenuItem;
+    MenuItem14: TMenuItem;
+    MenuItem15: TMenuItem;
+    MenuItem16: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    PayBtn: TButton;
+    RetourBtn: TButton;
+    NewCustomerBtn: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
+    Button18: TButton;
+    Button19: TButton;
+    Button20: TButton;
+    Button21: TButton;
+    Button22: TButton;
+    Button23: TButton;
+    Button24: TButton;
+    Button25: TButton;
+    Button26: TButton;
+    Button27: TButton;
+    Button28: TButton;
+    Button29: TButton;
+    Button30: TButton;
+    Button31: TButton;
+    Button32: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    ButtonPanel1: TButtonPanel;
+    FlowPanel1: TFlowPanel;
+    FlowPanel2: TFlowPanel;
+    FlowPanel3: TFlowPanel;
+    FlowPanel4: TFlowPanel;
+    FlowPanel5: TFlowPanel;
+    FlowPanel6: TFlowPanel;
+    FlowPanel7: TFlowPanel;
+    FlowPanel8: TFlowPanel;
+    FlowPanel9: TFlowPanel;
+    Shoppinglist: TGroupBox;
+    GroupBox3: TGroupBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    ListBox1: TListBox;
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    CancelPurchaseBtn: TButton;
+    ScrollBox1: TScrollBox;
+    StatusBar1: TStatusBar;
+    procedure MenuItem8Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.MenuItem8Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
